dac1ab000d82393c743ac4bf2e1e876d663b6167
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2026-04-02 22:15:40 +0530

    Deploying backend

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-
 require("dotenv").config({ override: true });
 // console.log("DEBUG MONGO_URL =", process.env.MONGO_URL);
 const cors = require("cors");
-const {getExploreProjects} = require("./controllers/project/getProjects")
+const { getExploreProjects } = require("./controllers/project/getProjects");
 
-
-const express=require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
-const cookieParser=require('cookie-parser');
-const http = require('http');
-const { Server } = require('socket.io');
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const userRouter=require('./routes/user');
-const projectRouter=require('./routes/project');
+const userRouter = require('./routes/user');
+const projectRouter = require('./routes/project');
 const authRouter = require('./routes/auth');
 
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
-const app=express();
+const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true
   }
 });
 
-const PORT=process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Atlas connected"));
 
-  // console.log(process.env.MONGO_URI);
-
+// console.log(process.env.MONGO_URI);
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/auth',authRouter);
+app.use('/auth', authRouter);
 
 // router.get("/explore", getExploreProjects);
 
 app.use(checkForAuthenticationCookie("token"));
 
-app.use('/user',userRouter);
-app.use('/project',projectRouter);
+app.use('/user', userRouter);
+app.use('/project', projectRouter);
 const taskRouter = require('./routes/task');
 app.use('/task', taskRouter);
 
@@ -122,4 +120,4 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT,()=>console.log(`server started at port: ${PORT}`));
+server.listen(PORT, () => console.log(`server started at port: ${PORT}`));
